65a86583b8f1797e79b78f5a8aba26827458da76
ranjana980 <ranjana20@navgurukul.org>
2023-06-10 16:44:46 +0530

    changes

--- index.js
+++ index.js
@@ -22,9 +22,9 @@ mongoos.connect("mongodb+srv://ranjana980:ranjana24081996@cluster0.glu3bgy.mongo
   }
 });
 
-// app.use(cors({
-//   origin: "https://mern-curd-application-frontend.vercel.app/"
-// }))
+app.use(cors({
+  origin: "https://mern-curd-application-frontend.vercel.app/"
+}))
 app.use(cors())
 
 app.use('/api/employee',EmployeeRoute)
