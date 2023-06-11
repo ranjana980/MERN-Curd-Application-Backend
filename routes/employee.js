ad6ac8a8976c8bd3ca7b13bf44e398b7d7c18c47
ranjana980 <ranjana20@navgurukul.org>
2023-06-11 14:38:22 +0530

    changes

--- routes/employee.js
+++ routes/employee.js
@@ -4,6 +4,6 @@ const EmployeeController = require('../Controller/EmployeeController')
 router.get('/', EmployeeController.index)
 router.get('/show/:id', EmployeeController.show)
 router.post('/store', EmployeeController.store)
-router.post('/update/:id', EmployeeController.update)
+router.patch('/update/:id', EmployeeController.update)
 router.delete('/delete/:id', EmployeeController.deleteEmpolyee)
 module.exports = router
