adb6d58b671630eb6c1bb5b84f0124e07ce226c9
ranjana2408 <ranjana20@navgurukul.org>
2024-04-06 22:55:50 +0530

    test

--- Controller/EmployeeController.js
+++ Controller/EmployeeController.js
@@ -1,5 +1,5 @@
 
-const bcrypt = require('bcrypt')
+// const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 const Validation = require('../Validation/formValid')
 const Employee = require('../models/employees')
